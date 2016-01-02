cb3cb1c527609e45a28876aa7aad31274bf90246
Tom Dickinson <tom@thomasmdickinson.com>
2016-01-02 11:19:26 -0500

    write pseudo code examples

---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,48 @@
+# 1.
+  # a method that returns the sum of two integers
+
+    # START
+    # GET first_integer
+    # GET second_integer
+    # SET sum = first_integer + second_integer
+    # PRINT sum
+    # END
+
+# 2.
+  # a method that takes an array of strings,
+  # and returns a string that is all those strings concatenated together
+
+    # START
+
+    # Given an array of strings called string_arr
+
+    # SET new_string = ""
+
+    # For each string in string_arr
+    #  new_string <<  string in string_arr
+
+    # PRINT new_string
+
+    # END
+
+# 3.
+  # a method that takes an array of integers,
+  # and returns a new array with every other element
+
+    # START
+
+    # Given an array of integers called int_arr
+
+    # SET new_arr = []
+
+    # For each integer in int_arr
+      # IF the integer's key in int arr is even (modulo 2 = 0)
+        # new_arr << integer in int_arr
+
+    # PRINT new_arr
+
+    # END
+
+
+
+    # END
